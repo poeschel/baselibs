da743f572846341f21f47b69a303aa53114fa529
Hanky Sjafrie <hanky.sjafrie.ext@qorix.ai>
2025-06-12 23:05:01 +0200

    add log_demo

---
+++ score/mw/log/examples/export.sh
@@ -0,0 +1,17 @@
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:\
+bazel-bin/score/mw/log:\
+bazel-bin/score/mw/log/configuration:\
+bazel-bin/score/mw/log/detail:\
+bazel-bin/score/mw/log/detail/file_logging:\
+bazel-bin/score/result:\
+bazel-bin/score/json:\
+bazel-bin/score/json/internal/parser/nlohmann:\
+bazel-bin/score/json/internal/writer/json_serialize:\
+bazel-bin/score/json/internal/model:\
+bazel-bin/score/filesystem:\
+bazel-bin/score/os:\
+bazel-bin/score/os/utils:\
+bazel-bin/score/memory:\
+bazel-bin/score/language/futurecpp"
+
+export LD_LIBRARY_PATH
--- score/mw/log/src/lib.rs
+++ score/mw/log/src/lib.rs
@@ -16,14 +16,14 @@ use std::ffi::c_char;
 
 unsafe extern "C" {
      fn mw_log_info(message: *const c_char);
-     fn mw_log_warning(message: *const c_char);
+     fn mw_log_warn(message: *const c_char);
      fn mw_log_error(message: *const c_char);
      fn mw_log_debug(message: *const c_char);
     // fn mw_log_fatal(message: *const c_char);
-     fn mw_log_trace(message: *const c_char);
+     fn mw_log_verbose(message: *const c_char);
  }
 
-#[warn(dead_code)]
+#[repr(C)]
 pub struct MwLogger;
 
 impl Log for MwLogger {
@@ -36,11 +36,11 @@ impl Log for MwLogger {
             let message = record.args().to_string();
             match record.level() {
                 Level::Error => unsafe { mw_log_error(message.as_ptr() as *const _) },
-                Level::Warn => unsafe { mw_log_warning(message.as_ptr() as *const _) },
+                Level::Warn => unsafe { mw_log_warn(message.as_ptr() as *const _) },
                 Level::Info => unsafe { mw_log_info(message.as_ptr() as *const _) },
                 // Level::Fatal => unsafe { mw_log_fatal(message.as_ptr() as *const _) },
                 Level::Debug => unsafe { mw_log_debug(message.as_ptr() as *const _) },
-                Level::Trace => unsafe { mw_log_trace(message.as_ptr() as *const _) },
+                Level::Trace => unsafe { mw_log_verbose(message.as_ptr() as *const _) },
             }
         }
     
